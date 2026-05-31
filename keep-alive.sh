043d42841ee8bca30673f668e1d7eea7ec2a3bbf
Z User <z@container>
2026-05-31 19:51:05 +0000

    5eeb11f0-9e82-40b1-b9b5-1ac2fd7c97c4

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+cd /home/z/my-project
 while true; do
-  if ! lsof -i :3000 >/dev/null 2>&1; then
-    echo "[$(date)] Restarting dev server..." >> /home/z/my-project/dev-restart.log
-    cd /home/z/my-project
-    npx next dev -p 3000 >> /home/z/my-project/dev.log 2>&1 &
-    sleep 5
+  bun run dev > dev.log 2>&1 &
+  SERVER_PID=$!
+  echo "[$(date)] Starting dev server (PID: $SERVER_PID)" >> keep-alive.log
+  sleep 5
+  # Check if it's still running
+  if kill -0 $SERVER_PID 2>/dev/null; then
+    echo "[$(date)] Server running (PID: $SERVER_PID)" >> keep-alive.log
+    # Wait for it to die
+    wait $SERVER_PID 2>/dev/null
+    echo "[$(date)] Server died (PID: $SERVER_PID), restarting..." >> keep-alive.log
+  else
+    echo "[$(date)] Server failed to start, retrying..." >> keep-alive.log
   fi
-  sleep 3
+  sleep 2
 done
